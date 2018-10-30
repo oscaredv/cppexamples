6ce342d090b95e6d27a5a987fce647fffa313be8
Oscar Endre Edvardsen <oscar.edvardsen@rolls-royce.com>
2018-10-30 11:36:11 +0100

    Improved filesystem example

--- filesystem.cpp
+++ filesystem.cpp
@@ -1,14 +1,17 @@
 // Examples using the filesystem library
 // Requires c++17
+#include <experimental/filesystem>
 #include <iostream>
 #include <iomanip>
-#include <experimental/filesystem>
-#include <map>
 #include <chrono>
 #include <fstream>
 #include <array>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <cerrno>
 
 using namespace std;
 using namespace std::chrono;
@@ -51,7 +54,7 @@ char listType(const fs::file_status& status)
     return ret;
 }
 
-std::string listPerms(fs::perms perms)
+string listPerms(fs::perms perms)
 {
     string ret("rwxrwxrwx");
     if ((perms & fs::perms::owner_read) == fs::perms::none)
@@ -136,7 +139,7 @@ void listFile(const fs::path& file)
     }
 
     auto lastWriteTime = system_clock::to_time_t(fs::last_write_time(file));
-    auto tm = std::localtime(&lastWriteTime);
+    auto tm = localtime(&lastWriteTime);
     cout << " " << put_time(tm, "%b.  %e %H:%M");
 
     cout << " " << filename << endl;
@@ -150,7 +153,7 @@ void listDir(const fs::path& dir)
         entries.push_back(entry);
     }
 
-    std::sort(entries.begin(), entries.end());
+    sort(entries.begin(), entries.end());
 
     for (auto& entry : entries)
     {
@@ -170,6 +173,44 @@ void list(const fs::path& path)
     }
 }
 
+string humanReadableSize(uintmax_t size)
+{
+    unsigned int suffixIndex = 0;
+    auto humanSize = size;
+    array<char, 5> suffix =
+    { ' ', 'K', 'M', 'G', 'T' };
+
+    while (humanSize >= 1024 && suffixIndex < (suffix.size() - 1U))
+    {
+        humanSize /= 1024;
+        suffixIndex++;
+    }
+
+    stringstream ret;
+    ret << humanSize;
+
+    if (suffixIndex > 0)
+    {
+        ret << suffix.at(suffixIndex);
+    }
+    return ret.str();
+}
+
+// The filesystem library lacks mkdtemp() functionality, so I wrote my own. Hopefully this is resolved in C++20
+fs::path& mkdtemp(fs::path& dir)
+{
+    string name(dir.string());
+    char tmp[PATH_MAX];
+    strncpy(tmp, name.c_str(), PATH_MAX); // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
+    if (::mkdtemp(tmp) == nullptr) // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
+    {
+        error_code ec = make_error_code(static_cast<errc>(errno));
+        throw fs::filesystem_error("Unable to create directory", ec);
+    }
+    dir = tmp;
+    return dir;
+}
+
 int main(int argc, char **argv)
 {
     (void) argc;
@@ -177,8 +218,66 @@ int main(int argc, char **argv)
 
     try
     {
-        list(".");
-    } catch (const std::exception& e)
+
+        // Space info
+        cout << "Available space on filesystem containing system temporary directory:" << endl;
+        fs::space_info si = fs::space(fs::temp_directory_path());
+        cout << fs::temp_directory_path().string() << ": available=" << humanReadableSize(si.available) << " capacity="
+                << humanReadableSize(si.capacity) << " free=" << humanReadableSize(si.free) << endl;
+
+        // Get system temp directory
+        fs::path tmpDir(fs::temp_directory_path());
+
+        // Operators / or /= works as directory separators
+        tmpDir /= "fs_XXXXXX";
+
+        // The filesystem library lacks mkstemp() and mkdtemp() functionality, so I wrote my own. Hopefully this is
+        // resolved in C++20
+        cout << "Create temporary directory: " << mkdtemp(tmpDir) << endl;
+
+        // Use the / operator on tmpDir to create a filename
+        fs::path fooPath(tmpDir / "foo.txt");
+        cout << "Create file " << fooPath << endl;
+        {
+            // Create file. ofstream now accepts filesystem::path as parameter instead of const char *
+            ofstream foo(fooPath);
+            foo << "foo\n";
+        }
+
+        cout << "List directory" << endl;
+        list(tmpDir);
+
+        // Rename file
+        fs::path barPath(tmpDir / "bar.txt");
+        cout << "Rename " << fooPath << " to " << barPath << endl;
+        fs::rename(fooPath, barPath);
+
+        // Create symlink
+        fs::path linkPath(tmpDir / "link");
+        cout << "Create symlink to bar: " << linkPath << endl;
+        fs::create_symlink(barPath, linkPath);
+
+        cout << "List directory" << endl;
+        list(tmpDir);
+
+        // Read file through link
+        cout << "Read file through link " << linkPath << endl;
+        ifstream in(linkPath);
+        char c = in.get();
+        while (in.good())
+        {
+            cout << c;
+            c = in.get();
+        }
+        in.close();
+
+        cout << "Remove " << linkPath << endl;
+        fs::remove(linkPath);
+        cout << "Remove " << barPath << endl;
+        fs::remove(barPath);
+        cout << "Remove " << tmpDir << endl;
+        fs::remove(tmpDir);
+    } catch (const exception& e)
     {
         cerr << e.what() << endl;
         exit(EXIT_FAILURE);
