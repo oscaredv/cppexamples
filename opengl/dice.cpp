0def4328801557d9bc7c585bdab15e67b60878cf
Oscar Endre Edvardsen <oscar.edvardsen@gmail.com>
2024-10-27 10:01:54 +0100

    Added dice corners to OpenGL example

--- opengl/dice.cpp
+++ opengl/dice.cpp
@@ -17,13 +17,13 @@ Dice::Dice() {
     float cos10 = cos(r) / 10.0f;
     float sin5 = sin(r) / 5.0f;
     float cos5 = cos(r) / 5.0f;
-    texture_[i] = std::make_pair(0.1f + sin10, 0.9f + cos10);
-    front_[i] = std::make_tuple(-0.8f + sin5, 0.8f + cos5, 1.0f);
-    top_[i] = std::make_tuple(0.8f - sin5, 1.0f, 0.8f + cos5);
-    bottom_[i] = std::make_tuple(-0.8f + sin5, -1.0f, 0.8f + cos5);
-    back_[i] = std::make_tuple(0.8f - sin5, 0.8f + cos5, -1.0f);
-    left_[i] = std::make_tuple(-1.0f, 0.8f - sin5, 0.8f + cos5);
-    right_[i] = std::make_tuple(1.0f, -0.8f + sin5, 0.8f + cos5);
+    texture_[i] = {0.1f + sin10, 0.9f + cos10};
+    front_[i] = {-0.8f + sin5, 0.8f + cos5, 1.0f};
+    top_[i] = {0.8f - sin5, 1.0f, 0.8f + cos5};
+    bottom_[i] = {-0.8f + sin5, -1.0f, 0.8f + cos5};
+    back_[i] = {0.8f - sin5, 0.8f + cos5, -1.0f};
+    left_[i] = {-1.0f, 0.8f - sin5, 0.8f + cos5};
+    right_[i] = {1.0f, -0.8f + sin5, 0.8f + cos5};
     i++;
   }
   for (int d = 0; d <= 90; d += 18) {
@@ -32,13 +32,13 @@ Dice::Dice() {
     float cos10 = cos(r) / 10.0f;
     float sin5 = sin(r) / 5.0f;
     float cos5 = cos(r) / 5.0f;
-    texture_[i] = std::make_pair(0.9f + sin10, 0.9f + cos10);
-    front_[i] = std::make_tuple(0.8f + sin5, 0.8f + cos5, 1.0f);
-    top_[i] = std::make_tuple(-0.8f - sin5, 1.0f, 0.8f + cos5);
-    bottom_[i] = std::make_tuple(0.8f + sin5, -1.0f, 0.8f + cos5);
-    back_[i] = std::make_tuple(-0.8f - sin5, 0.8f + cos5, -1.0f);
-    left_[i] = std::make_tuple(-1.0f, -0.8f - sin5, 0.8f + cos5);
-    right_[i] = std::make_tuple(1.0f, 0.8f + sin5, 0.8f + cos5);
+    texture_[i] = {0.9f + sin10, 0.9f + cos10};
+    front_[i] = {0.8f + sin5, 0.8f + cos5, 1.0f};
+    top_[i] = {-0.8f - sin5, 1.0f, 0.8f + cos5};
+    bottom_[i] = {0.8f + sin5, -1.0f, 0.8f + cos5};
+    back_[i] = {-0.8f - sin5, 0.8f + cos5, -1.0f};
+    left_[i] = {-1.0f, -0.8f - sin5, 0.8f + cos5};
+    right_[i] = {1.0f, 0.8f + sin5, 0.8f + cos5};
     i++;
   }
   for (int d = 90; d <= 180; d += 18) {
@@ -47,13 +47,13 @@ Dice::Dice() {
     float cos10 = cos(r) / 10.0f;
     float sin5 = sin(r) / 5.0f;
     float cos5 = cos(r) / 5.0f;
-    texture_[i] = std::make_pair(0.9f + sin10, 0.1f + cos10);
-    front_[i] = std::make_tuple(0.8f + sin5, -0.8f + cos5, 1.0f);
-    top_[i] = std::make_tuple(-0.8f - sin5, 1.0f, -0.8f + cos5);
-    bottom_[i] = std::make_tuple(0.8f + sin5, -1.0f, -0.8f + cos5);
-    back_[i] = std::make_tuple(-0.8f - sin5, -0.8f + cos5, -1.0f);
-    left_[i] = std::make_tuple(-1.0f, -0.8f - sin5, -0.8f + cos5);
-    right_[i] = std::make_tuple(1.0f, 0.8f + sin5, -0.8f + cos5);
+    texture_[i] = {0.9f + sin10, 0.1f + cos10};
+    front_[i] = {0.8f + sin5, -0.8f + cos5, 1.0f};
+    top_[i] = {-0.8f - sin5, 1.0f, -0.8f + cos5};
+    bottom_[i] = {0.8f + sin5, -1.0f, -0.8f + cos5};
+    back_[i] = {-0.8f - sin5, -0.8f + cos5, -1.0f};
+    left_[i] = {-1.0f, -0.8f - sin5, -0.8f + cos5};
+    right_[i] = {1.0f, 0.8f + sin5, -0.8f + cos5};
     i++;
   }
   for (int d = 180; d <= 270; d += 18) {
@@ -62,13 +62,13 @@ Dice::Dice() {
     float cos10 = cos(r) / 10.0f;
     float sin5 = sin(r) / 5.0f;
     float cos5 = cos(r) / 5.0f;
-    texture_[i] = std::make_pair(0.1f + sin10, 0.1f + cos10);
-    front_[i] = std::make_tuple(-0.8f + sin5, -0.8f + cos5, 1.0f);
-    top_[i] = std::make_tuple(0.8f - sin5, 1.0f, -0.8f + cos5);
-    bottom_[i] = std::make_tuple(-0.8f + sin5, -1.0f, -0.8f + cos5);
-    back_[i] = std::make_tuple(0.8f - sin5, -0.8f + cos5, -1.0f);
-    left_[i] = std::make_tuple(-1.0, 0.8f - sin5, -0.8f + cos5);
-    right_[i] = std::make_tuple(1.0, -0.8f + sin5, -0.8f + cos5);
+    texture_[i] = {0.1f + sin10, 0.1f + cos10};
+    front_[i] = {-0.8f + sin5, -0.8f + cos5, 1.0f};
+    top_[i] = {0.8f - sin5, 1.0f, -0.8f + cos5};
+    bottom_[i] = {-0.8f + sin5, -1.0f, -0.8f + cos5};
+    back_[i] = {0.8f - sin5, -0.8f + cos5, -1.0f};
+    left_[i] = {-1.0, 0.8f - sin5, -0.8f + cos5};
+    right_[i] = {1.0, -0.8f + sin5, -0.8f + cos5};
     i++;
   }
 }
@@ -76,75 +76,78 @@ Dice::Dice() {
 void Dice::draw() {
   glEnable(GL_TEXTURE_2D);
 
-  // Front face
-  glBindTexture(GL_TEXTURE_2D, textures_[5]);
-  glBegin(GL_TRIANGLE_FAN);
-  glNormal3f(0.0f, 0.0f, 1.0f);
-  for (int i = 0; i < 24; i++) {
-    auto [tx, ty] = texture_[i];
-    auto [x, y, z] = front_[i];
-    glTexCoord2f(tx, ty);
-    glVertex3f(x, y, z);
-  }
-  glEnd();
+  // Rounded faces
+  draw_face(textures_[5], {0.0f, 0.0f, 1.0f}, front_);
+  draw_face(textures_[0], {0.0f, 0.0f, -1.0f}, back_);
+  draw_face(textures_[2], {0.0f, 1.0f, 0.0f}, top_);
+  draw_face(textures_[3], {0.0f, -1.0f, 0.0f}, bottom_);
+  draw_face(textures_[4], {1.0f, 0.0f, 0.0f}, right_);
+  draw_face(textures_[1], {-1.0f, 0.0f, 0.0f}, left_);
 
-  // Back face
-  glBindTexture(GL_TEXTURE_2D, textures_[0]);
-  glBegin(GL_TRIANGLE_FAN);
-  glNormal3f(0.0f, 0.0f, -1.0f);
-  for (int i = 0; i < 24; i++) {
-    auto [tx, ty] = texture_[i];
-    auto [x, y, z] = back_[i];
-    glTexCoord2f(tx, ty);
-    glVertex3f(x, y, z);
-  }
-  glEnd();
+  // Corner 1
+  draw_corner({-1.0f, 1.0f, 1.0f}, front_, 0);
+  draw_corner({-1.0f, 1.0f, 1.0f}, top_, 6);
+  draw_corner({-1.0f, 1.0f, 1.0f}, left_, 0);
 
-  // Top face
-  glBindTexture(GL_TEXTURE_2D, textures_[2]);
-  glBegin(GL_TRIANGLE_FAN);
-  glNormal3f(0.0f, 1.0f, 0.0f);
-  for (int i = 0; i < 24; i++) {
-    auto [tx, ty] = texture_[i];
-    auto [x, y, z] = top_[i];
-    glTexCoord2f(tx, ty);
-    glVertex3f(x, y, z);
-  }
-  glEnd();
+  // Corner 2
+  draw_corner({1.0f, 1.0f, 1.0f}, front_, 6);
+  draw_corner({1.0f, 1.0f, 1.0f}, top_, 0);
+  draw_corner({1.0f, 1.0f, 1.0f}, right_, 6);
 
-  // Bottom face
-  glBindTexture(GL_TEXTURE_2D, textures_[3]);
-  glBegin(GL_TRIANGLE_FAN);
-  glNormal3f(0.0f, -1.0f, 0.0f);
-  for (int i = 0; i < 24; i++) {
-    auto [tx, ty] = texture_[i];
-    auto [x, y, z] = bottom_[i];
-    glTexCoord2f(tx, ty);
-    glVertex3f(x, y, z);
-  }
-  glEnd();
+  // Corner 3
+  draw_corner({1.0f, -1.0f, 1.0f}, front_, 12);
+  draw_corner({1.0f, -1.0f, 1.0f}, bottom_, 6);
+  draw_corner({1.0f, -1.0f, 1.0f}, right_, 0);
 
-  // Right face
-  glBindTexture(GL_TEXTURE_2D, textures_[4]);
-  glBegin(GL_TRIANGLE_FAN);
-  glNormal3f(1.0f, 0.0f, 0.0f);
-  for (int i = 0; i < 24; i++) {
-    auto [tx, ty] = texture_[i];
-    auto [x, y, z] = right_[i];
-    glTexCoord2f(tx, ty);
-    glVertex3f(x, y, z);
-  }
-  glEnd();
+  // Corner 4
+  draw_corner({-1.0f, -1.0f, 1.0f}, front_, 18);
+  draw_corner({-1.0f, -1.0f, 1.0f}, bottom_, 0);
+  draw_corner({-1.0f, -1.0f, 1.0f}, left_, 6);
 
-  // Left face
-  glBindTexture(GL_TEXTURE_2D, textures_[1]);
+  // Corner 5
+  draw_corner({1.0f, 1.0f, -1.0f}, back_, 0);
+  draw_corner({1.0f, 1.0f, -1.0f}, top_, 18);
+  draw_corner({1.0f, 1.0f, -1.0f}, right_, 12);
+
+  // Corner 6
+  draw_corner({-1.0f, 1.0f, -1.0f}, back_, 6);
+  draw_corner({-1.0f, 1.0f, -1.0f}, top_, 12);
+  draw_corner({-1.0f, 1.0f, -1.0f}, left_, 18);
+
+  // Corner 7
+  draw_corner({-1.0f, -1.0f, -1.0f}, back_, 12);
+  draw_corner({-1.0f, -1.0f, -1.0f}, bottom_, 18);
+  draw_corner({-1.0f, -1.0f, -1.0f}, left_, 12);
+
+  // Corner 8
+  draw_corner({1.0f, -1.0f, -1.0f}, back_, 18);
+  draw_corner({1.0f, -1.0f, -1.0f}, bottom_, 12);
+  draw_corner({1.0f, -1.0f, -1.0f}, right_, 18);
+}
+
+void Dice::draw_face(GLuint texture_id, const std::array<float, 3> &normal,
+                     const std::array<std::array<float, 3>, 24> &coords) {
+  glBindTexture(GL_TEXTURE_2D, texture_id);
+  glNormal3fv(normal.data());
   glBegin(GL_TRIANGLE_FAN);
-  glNormal3f(-1.0f, 0.0f, 0.0f);
   for (int i = 0; i < 24; i++) {
-    auto [tx, ty] = texture_[i];
-    auto [x, y, z] = left_[i];
-    glTexCoord2f(tx, ty);
-    glVertex3f(x, y, z);
+    glTexCoord2fv(texture_[i].data());
+    glVertex3fv(coords[i].data());
   }
   glEnd();
+}
+
+void Dice::draw_corner(const std::array<float, 3> &normal,
+                       const std::array<std::array<float, 3>, 24> &coords,
+                       int coords_offset) {
+  glNormal3fv(normal.data());
+  std::array<float, 3> corner = {normal[0] * 0.97f, normal[1] * 0.97f,
+                                 normal[2] * 0.97f};
+  for (int i = 0; i < 5; i++) {
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex3fv(corner.data());
+    glVertex3fv(coords[coords_offset + i + 1].data());
+    glVertex3fv(coords[coords_offset + i].data());
+    glEnd();
+  }
 }
--- opengl/dice.h
+++ opengl/dice.h
@@ -12,14 +12,21 @@ public:
 
   void draw() override;
 
+protected:
+  void draw_face(GLuint texture_id, const std::array<float, 3> &normal,
+                 const std::array<std::array<float, 3>, 24> &coords);
+  void draw_corner(const std::array<float, 3> &normal,
+                   const std::array<std::array<float, 3>, 24> &coords,
+                   int coords_offset);
+
 private:
   std::array<GLuint, 6> textures_;
 
-  std::array<std::pair<float, float>, 24> texture_;
-  std::array<std::tuple<float, float, float>, 24> front_;
-  std::array<std::tuple<float, float, float>, 24> back_;
-  std::array<std::tuple<float, float, float>, 24> top_;
-  std::array<std::tuple<float, float, float>, 24> bottom_;
-  std::array<std::tuple<float, float, float>, 24> left_;
-  std::array<std::tuple<float, float, float>, 24> right_;
+  std::array<std::array<float, 2>, 24> texture_;
+  std::array<std::array<float, 3>, 24> front_;
+  std::array<std::array<float, 3>, 24> back_;
+  std::array<std::array<float, 3>, 24> top_;
+  std::array<std::array<float, 3>, 24> bottom_;
+  std::array<std::array<float, 3>, 24> left_;
+  std::array<std::array<float, 3>, 24> right_;
 };
